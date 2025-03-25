86a51a92cbe89a24d48da717096369b5c60640e4
Jose Garcia <josegarcia.dev@gmail.com>
2025-03-25 16:50:36 -0600

    Components placed in grid.

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,26 +3,29 @@ import { Toast, Form, LoadingSpin } from "@monorepo/ui";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-custom-light">
+	  <>
 		<div className="flex flex-col items-center justify-center">
 			<h1 className="text-4xl font-bold text-center">AWKDevs</h1>
 		</div>
 
-		<div className="min-h-screen flex flex-col justify-center items-center">
+    <div className="grid grid-rows-1 grid-cols-3 gap-4 items-center justify-items-center min-h-screen sm:p-20 bg-custom-dark">
+
+		<div className="min-h-screen flex flex-col justify-center items-center bg-(--custom-dark) text-(--custom-light) p-8">
 		  <h1 className="text-2xl font-bold mb-4">Cargando...</h1>
 			<LoadingSpin/>
 		</div>
 
-		<div className="min-h-screen flex flex-col justify-center items-center">
+		<div className="min-h-screen flex flex-col justify-center items-center bg-(--custom-dark) text-(--custom-light) p-8">
 		  <h1 className="text-2xl font-bold mb-4">Toast</h1>
 			<Toast/>
 		</div>
 
-		<div className="min-h-screen flex flex-col justify-center items-center">
+		<div className="min-h-screen flex flex-col justify-center items-center bg-(--custom-dark) text-(--custom-light) p-8">
 		  <h1 className="text-2xl font-bold mb-4">Form</h1>
 			<Form/>
 		</div>
 
     </div>
+	</>
   );
 }
